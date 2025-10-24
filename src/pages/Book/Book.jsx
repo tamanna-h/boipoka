686bffca0232e6cc992db3a11856ba519f01b51c
Tamanna <tahminadox@gmail.com>
2025-10-24 10:19:58 +0600

    Wrap BookCard component with React Link to dynamically pass bookId

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,5 +1,6 @@
 import React, { use } from 'react';
 import { FaStarHalfAlt } from "react-icons/fa";
+import { Link } from 'react-router';
 
 const Book = ({book}) => {
     // const data = use(bookPromise);
@@ -7,10 +8,12 @@ const Book = ({book}) => {
     // console.log(data);
     
     console.log(book);
-    const {bookName, author, image, rating, category, tags, yearOfPublishing, publisher} = book
+    const {bookName, author, bookId, image, rating, category, tags, yearOfPublishing, publisher} = book
     
     return (
-        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+        <Link to={`/bookDetails/${bookId}`
+        }>
+          <div className="card bg-base-100 w-96 shadow-sm border p-6">
   <figure className="p-10 bg-gray-600 w-2/3 mx-auto">
     <img className='h-[166px]'
       src={image}
@@ -27,13 +30,14 @@ const Book = ({book}) => {
       <div className="badge badge-secondary">{yearOfPublishing}</div>
     </h2>
     <p>Book by: {publisher}</p>
-    <div className='border-t-1 border-dashed'></div>
+    <div className='border-t border-dashed'></div>
     <div className="card-actions justify-end">
       <div className="badge badge-outline">{category}</div>
       <div className="badge badge-outline">{rating}<FaStarHalfAlt /></div>
     </div>
   </div>
 </div>
+        </Link>
     );
 };
 
