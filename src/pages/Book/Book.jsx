13717ac63a6fb142559638ead2cd70bee08abfcc
Tamanna <tahminadox@gmail.com>
2025-10-24 09:38:20 +0600

    Polished the product card dynamicly and added React Icons

--- src/pages/Book/Book.jsx
+++ src/pages/Book/Book.jsx
@@ -1,4 +1,5 @@
 import React, { use } from 'react';
+import { FaStarHalfAlt } from "react-icons/fa";
 
 const Book = ({book}) => {
     // const data = use(bookPromise);
@@ -6,24 +7,24 @@ const Book = ({book}) => {
     // console.log(data);
     
     console.log(book);
-    const {bookName, author, image} = book
+    const {bookName, author, image, rating, category} = book
     
     return (
-        <div className="card bg-base-100 w-96 shadow-sm">
-  <figure className="px-10 pt-10">
-    <img
+        <div className="card bg-base-100 w-96 shadow-sm border p-6">
+  <figure className="p-10 bg-gray-600 w-2/3 mx-auto">
+    <img className='h-[166px]'
       src={image}
       alt="Shoes" />
   </figure>
   <div className="card-body">
     <h2 className="card-title">
-      Card Title
+      {bookName}
       <div className="badge badge-secondary">NEW</div>
     </h2>
     <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <div className="badge badge-outline">{category}</div>
+      <div className="badge badge-outline">{rating}<FaStarHalfAlt /></div>
     </div>
   </div>
 </div>
--- src/pages/Books/Books.jsx
+++ src/pages/Books/Books.jsx
@@ -13,7 +13,7 @@ const Books = ({data}) => {
     // const bookPromise = fetch('booksData.json').then(res => res.json())
 
     return (
-        <div>
+        <div className='max-w-7xl mx-auto'>
             <h1 className='text-3xl text-center p-6'>All Books</h1>
             <Suspense fallback={<div>Loading...</div>}>
                 <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
